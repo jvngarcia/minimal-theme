d7762d0b72740e5c6387e4f3c8d7d87bc9b7661b
Angel Garcia <a.garcia@bancamiga.com>
2022-10-03 16:38:14 -0400

    Cards created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.general')
 
+@section('css')
+    @vite(['resources/css/posts.css'])
+@endsection
+
 @section('title', 'Home')
 
 @section('content')
-Hello
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 py-3">
+            <div class="card mx-1">
+                <img src="https://unavatar.io/kikobeats" class="card-img-top img-fluid" alt="title" />
+                <div class="card-body">
+                    <span class="card-category">CATEGORIA</span>
+                    <h6 class="card-title">{title}</h6>
+                    <div class="d-flex justify-content-between">
+                        <p class="card-text card-author mb-0">{ user }</p>
+                        <p class="card-text card-date">{ date }</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
