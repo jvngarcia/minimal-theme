9cf12e285257e083e71c23c409872157ff764a14
Angel Garcia <a.garcia@bancamiga.com>
2022-10-03 16:37:19 -0400

    Posts styles

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/posts.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
