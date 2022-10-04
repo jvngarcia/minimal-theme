520033cfc7ab7c7f31bea4055f8200c953b730f0
Angel Garcia <a.garcia@bancamiga.com>
2022-10-04 09:54:32 -0400

    Import: Poppins and Roboto

--- resources/views/layouts/header-principal.blade.php
+++ resources/views/layouts/header-principal.blade.php
@@ -2,19 +2,19 @@
     <nav class="navbar navbar-expand-lg bg-white py-3">
         <div class="container-fluid mx-sm-4">
             <div class='container'>
-            <div class='row w-100'>
-                <div class='col-sm-4'>
+            <div class='row'>
+                <div class='col-md-4 d-md-block justify-content-md-start d-flex justify-content-center'>
                     <a class="navbar-brand" href="{{ route('home') }}">
                         LOGO
                     </a>
                 </div>
 
-                <div class='col-sm-4 d-flex justify-content-center'>
-                <form class="w-100" role="search">
-                    <input class="form-control me-1" type="search" placeholder="Buscar noticia..." aria-label="Bucar noticia" />
-                </form>
+                <div class='col-md-4 d-flex justify-content-center mt-md-0 mt-3'>
+                    <form class="w-100" role="search">
+                        <input class="form-control me-1" type="search" placeholder="Buscar noticia..." aria-label="Bucar noticia" />
+                    </form>
                 </div>
-                <div class='col-sm-4 d-flex justify-content-end align-items-center'>
+                <div class='col-md-4 d-flex justify-content-end align-items-center'>
                     
                 </div>
             </div>
