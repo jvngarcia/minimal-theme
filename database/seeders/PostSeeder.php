f3fa15cfdc5bffcc477010c0f52ecc88017f5a84
Angel Garcia <angelgarciaweb@gmail.com>
2023-05-08 16:52:58 -0400

    ✨ feat: agregar post base

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -19,22 +19,64 @@ class PostSeeder extends Seeder
 
 
         Post::create([
-            'slug' => 'Lorem-ipsun-dolore',
-            'name' => 'Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd',
-            'extract' => 'Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd',
-            'content' => 'Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd.',
-            'image' => 'https://unavatar.io/kikobeats',
-            'category' => 1,
+            'slug' => 'peluqueria-canina-una-guia-para-nuevos-duenos-de-perros',
+            'name' => 'Peluquería canina: una guía para nuevos dueños de perros',
+            'extract' => 'Los perros son compañeros increíbles, pero requieren de cierta cantidad de cuidado y preparación. ¿Estás preparado?',
+            'content' => "# Peluquería canina: una guía para nuevos dueños de perros
+¡Felicitaciones por tu nuevo perro! Los perros son compañeros increíbles, pero requieren una cierta cantidad de cuidado y preparación. Este artículo le proporcionará una descripción general básica del cuidado de un perro, incluido lo que debe hacer, la frecuencia con la que debe hacerlo y algunos consejos para facilitar el proceso tanto para usted como para su perro.
+## ¿Qué es la peluquería canina?
+El aseo canino es el proceso de mantener el pelaje de tu perro limpio, saludable y libre de esteras. También incluye bañarse, cepillarse, cortarse las uñas y limpiarse los oídos. La frecuencia del cepillado variará según la raza, el tipo de pelaje y el nivel de actividad de su perro. Por ejemplo, es posible que un perro de pelo corto solo deba bañarse una vez al mes, mientras que un perro de pelo largo puede necesitar un baño semanal.
+## ¿Por qué es importante la peluquería canina?
+Hay varias razones por las que el cuidado del perro es importante. En primer lugar, ayuda a mantener el pelaje de tu perro limpio y libre de esteras. Esto es importante tanto para la salud de tu perro como para la tuya. Los tapetes pueden causar irritación e infección en la piel, y también pueden ser dolorosos para tu perro cuando se mueve.
+En segundo lugar, el aseo del perro ayuda a eliminar la suciedad, los desechos y los parásitos del pelaje de su perro. Esto puede ayudar a prevenir la propagación de enfermedades y mantener a su perro saludable.
+En tercer lugar, el aseo canino puede ayudar a mejorar la apariencia de su perro. Es más probable que un perro bien cuidado sea confiado y extrovertido.
+## Cómo acicalar a tu perro
+Hay algunas cosas básicas que deberá hacer para acicalar a su perro. Éstas incluyen:
+- Baño: Bañar a su perro ayuda a eliminar la suciedad, los desechos y el pelo suelto. También ayuda a mantener la piel de tu perro limpia y saludable.
+- Cepillado: cepillar el pelaje de su perro ayuda a eliminar las marañas y los enredos. También ayuda a distribuir aceites naturales por todo el pelaje, lo que ayuda a mantenerlo saludable.
+- Recortar las uñas: Recortar las uñas de tu perro ayuda a evitar que se agrieten o rompan. También ayuda a evitar que tu perro te rasque a ti o a tus muebles.
+- Limpieza de oídos: Limpiar los oídos de su perro ayuda a eliminar la suciedad, los desechos y la cera. También ayuda a prevenir infecciones de oído.
+## Consejos para acicalar a tu perro
+Aquí hay algunos consejos para facilitar el proceso de aseo tanto para usted como para su perro:
+- Comienza a cepillar a tu perro cuando sea un cachorro. Esto les ayudará a acostumbrarse al proceso y te lo hará más fácil a ti cuando sean mayores.
+- Haz del aseo una experiencia positiva para tu perro. Use refuerzos positivos, como golosinas o elogios, para que su perro asocie el aseo con cosas buenas.
+- Ser paciente. El aseo puede ser un proceso lento, especialmente para los perros que no están acostumbrados. Sea paciente y tómese su tiempo.
+- Si no se siente cómodo acicalando a su perro usted mismo, siempre puede llevarlo a un peluquero profesional.
+## Conclusión
+El cuidado del perro es una parte importante de tener un perro. Ayuda a mantener a su perro limpio, saludable y luciendo lo mejor posible. Si sigue los consejos de este artículo, puede facilitar el proceso de aseo tanto para usted como para su perro.",
+            'image' => 'https://gbzxvkaqkoonnjdjgzbq.supabase.co/storage/v1/object/public/ecosistema-canino/blog/una-guia-para-nuevos-duenos-de-perros.png?t=2023-05-08T20%3A15%3A08.400Z',
+            'category' => 2,
             'author' => 1
         ]);
 
         Post::create([
-            'slug' => 'Lorem-ipsun-dolore-sit-amet',
-            'name' => 'Lorem ipsun dolore sit a met dor me practice',
+            'slug' => 'pitbull-care-una-guia-para-nuevos-duenos',
+            'name' => 'Pitbull Care: una guía para nuevos dueños',
             'extract' => 'Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd',
-            'content' => 'Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, Lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd, lorem ipsun dolore sit a met dor me practice loremasd asd asd asdasd asd.',
-            'image' => 'https://unavatar.io/kikobeats',
-            'category' => 1,
+            'content' => 'Los pitbulls son perros maravillosos, pero requieren una cierta cantidad de cuidado y preparación. Este artículo le proporcionará una descripción general básica del cuidado de Pitbull o perros tipo bull, incluido lo que debe hacer, la frecuencia con la que debe hacerlo y algunos consejos para facilitar el proceso tanto para usted como para su perro.
+## ¿Qué es Pitbull Care?
+El cuidado de Pitbull es el proceso de mantener a su Pitbull sano, feliz y con buen comportamiento. Incluye una variedad de tareas, como alimentación, aseo, ejercicio y entrenamiento. La frecuencia de estas tareas variará según la edad, la salud y el nivel de actividad de su Pitbull. Por ejemplo, un cachorro necesitará ser alimentado con más frecuencia que un perro adulto, y un perro de trabajo necesitará más ejercicio que un teleadicto.
+## ¿Por qué es importante el cuidado de Pitbull?
+Hay varias razones por las que el cuidado de Pitbull es importante. Primero, ayuda a mantener saludable a tu Pitbull. Al alimentar a su Pitbull con una dieta saludable, hacer ejercicio con regularidad y hacerles chequeos veterinarios regulares, puede ayudar a prevenir problemas de salud como la obesidad, las enfermedades cardíacas y el cáncer.
+En segundo lugar, el cuidado de Pitbull ayuda a mantener feliz a su Pitbull. Al brindarle a su perro mucho amor, atención y ejercicio, puede ayudar a garantizar que sea feliz y bien adaptado.
+En tercer lugar, el cuidado de Pitbull ayuda a que se comporte bien. Al entrenar a su Pitbull para que obedezca órdenes básicas, puede ayudar a garantizar que sea un compañero seguro y agradable.
+## Cómo cuidar a un pitbull
+Hay algunas cosas básicas que deberá hacer para cuidar a su Pitbull. Éstas incluyen:
+-   **Alimentación:** los pitbulls necesitan una dieta de alta calidad que sea rica en proteínas y otros nutrientes. Debe alimentar a su Pitbull con una dieta diseñada específicamente para perros de razas grandes.
+-   **Aseo:** los pitbulls tienen pelajes cortos y suaves que son relativamente fáciles de arreglar. Debes cepillar el pelaje de tu pitbull al menos una vez a la semana para eliminar el pelo suelto y evitar que se formen nudos. También debe bañar a su Pitbull según sea necesario, pero no más de una vez al mes.
+-   **Ejercicio:** los pitbulls son perros de mucha energía que necesitan mucho ejercicio. Debe pasear a su Pitbull durante al menos 30 minutos al día, y también debe brindarle muchas oportunidades para jugar y correr.
+-   **Entrenamiento:** Los pitbulls son perros inteligentes que son fáciles de entrenar. Debes comenzar a entrenar a tu pitbull desde que es un cachorro y debes continuar entrenándolo durante toda su vida. Los comandos básicos de obediencia, como sentarse, quedarse quieto y venir, son esenciales para todos los pitbulls.
+## Consejos para el cuidado de un pitbull
+Aquí hay algunos consejos para facilitar el proceso de cuidado de un Pitbull tanto para usted como para su perro:
+-   **Comience temprano:** cuanto antes comience a cuidar a su Pitbull, más fácil será. Comience a entrenar a su pitbull desde que es un cachorro y comience a socializarlo con otros perros y personas lo antes posible.
+-   **Sea constante:** Es importante ser constante con el cuidado de su Pitbull. Esto significa alimentarlos a la misma hora todos los días, llevarlos a caminar a la misma hora todos los días y entrenarlos regularmente.
+-   **Ten paciencia:** los pitbulls son perros inteligentes, pero también pueden ser testarudos. Sea paciente con su pitbull y no se desanime si no aprende algo de inmediato.
+-   **Sea positivo:** los pitbulls son muy sensibles al refuerzo positivo. Usa golosinas, elogios y caricias para recompensar a tu pitbull por su buen comportamiento.
+-   **Sea cariñoso:** los pitbulls son perros cariñosos y afectuosos. Muéstrale a tu Pitbull cuánto los amas y ellos te amarán a ti.
+## Conclusión
+El cuidado de Pitbull es una experiencia gratificante. Al seguir los consejos de este artículo, puede ayudar a garantizar que su Pitbull tenga una vida larga, saludable y feliz.',
+            'image' => 'https://gbzxvkaqkoonnjdjgzbq.supabase.co/storage/v1/object/public/ecosistema-canino/blog/guia-para-nuevos-duenos-de-pitbull.png?t=2023-05-08T20%3A37%3A49.786Z',
+            'category' => 2,
             'author' => 1
         ]);
 
