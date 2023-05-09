95c2df01d219e1b1ae36b79462dba46d77d6e98f
Angel Garcia <angelgarciaweb@gmail.com>
2023-05-09 09:43:13 -0400

    🎨 fix: ordenar código

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,8 @@ use App\Models\Post;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $posts = Post::all()->sortByDesc('created_at');
 
         return view('home', compact('posts'));
